6121064a860afc02bf7db2d5fb21ee180b42259b
Idriss <you@example.com>
2024-05-03 15:43:36 +0100

    added filter bar

--- soa-app/src/Components/Services/Home/home.jsx
+++ soa-app/src/Components/Services/Home/home.jsx
@@ -6,7 +6,13 @@ import "./home.css";
 function Home() {
   const [rooms, setRooms] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
-  const location = useLocation();
+  const [showFilters, setShowFilters] = useState(false);
+  const [minSize, setMinSize] = useState("");
+  const [maxSize, setMaxSize] = useState("");
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+  const [capacity, setCapacity] = useState("");
+  const [city, setCity] = useState("");
 
   useEffect(() => {
     // Fetch room data from the API
@@ -15,8 +21,6 @@ function Home() {
       .then((data) => setRooms(data))
       .catch((error) => console.error("Error fetching rooms:", error));
   }, []);
-
-  console.log("Rooms:", rooms);
 
   // Function to handle search
   const handleSearch = () => {
@@ -30,37 +34,117 @@ function Home() {
     setRooms(filteredRooms);
   };
 
+  // Function to handle filtering
+  const handleFilter = () => {
+    console.log(minSize, maxSize, minPrice, maxPrice, capacity, city);
+    const filteredRooms = rooms.filter((room) => {
+      return (
+        (!minSize || room.size >= parseInt(minSize)) &&
+        (!maxSize || room.size <= parseInt(maxSize)) &&
+        (!minPrice || room.price_per_hour >= parseInt(minPrice)) &&
+        (!maxPrice || room.price_per_hour <= parseInt(maxPrice)) &&
+        (!capacity || room.capacity >= parseInt(capacity)) &&
+        (!city || room.location.toLowerCase().includes(city.toLowerCase()))
+      );
+    });
+
+    setRooms(filteredRooms);
+    if (filteredRooms.length === 0) alert("No rooms found") && setRooms(rooms);
+    console.log(filteredRooms);
+  };
+
   // Function to handle page refresh
   const handleRefresh = () => {
     window.location.reload();
+  };
+
+  // Function to toggle filter visibility
+  const toggleFilters = () => {
+    setShowFilters(!showFilters);
   };
 
   return (
     <div className="bodyy">
       <div className="home">
         <h1>
-          <div className="searchbar">
-            <input
-              className="search-input"
-              variant="outlined"
-              fullWidth
-              label="Search"
-              placeholder="  Search..."
-              value={searchQuery} // Bind input value to searchQuery state
-              onChange={(e) => setSearchQuery(e.target.value)} // Update searchQuery on input change
-            />
-            <button
-              type="button"
-              className="search-button"
-              onClick={handleSearch} // Call handleSearch function on button click
-            >
-              Search
-            </button>
-            {/* Add the refresh button */}
-            <button className="search-button" onClick={handleRefresh}>
-              Refresh
-            </button>
-          </div>
+          {!showFilters && (
+            <div className="searchbar">
+              <input
+                className="search-input"
+                variant="outlined"
+                fullWidth
+                label="Search"
+                placeholder="  Search..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+              <button
+                type="button"
+                className="search-button"
+                onClick={handleSearch}
+              >
+                Search
+              </button>
+              <button className="search-button" onClick={handleRefresh}>
+                Refresh
+              </button>
+              <button className="search-button" onClick={toggleFilters}>
+                Show Filters
+              </button>
+            </div>
+          )}
+          {showFilters && (
+            <div className="filterbar">
+              <button className="search-button" onClick={toggleFilters}>
+                Hide Filters
+              </button>
+              <input
+                type="text"
+                placeholder=" Minimum Size in m2...."
+                className="search-input"
+                value={minSize}
+                onChange={(e) => setMinSize(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder=" Maximum Size in m2...."
+                className="search-input"
+                value={maxSize}
+                onChange={(e) => setMaxSize(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder=" Minimum price/hour...."
+                className="search-input"
+                value={minPrice}
+                onChange={(e) => setMinPrice(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder=" Maximum price/hour...."
+                className="search-input"
+                value={maxPrice}
+                onChange={(e) => setMaxPrice(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder=" Max Capacity...."
+                className="search-input"
+                value={capacity}
+                onChange={(e) => setCapacity(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder=" City...."
+                className="search-input"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+              <button className="filter-button" onClick={handleFilter}>
+                Filter
+              </button>
+            </div>
+          )}
         </h1>
 
         <div className="cards">
